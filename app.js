2c12d9187fa502cd307f22dbd8402c4cc0815448
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-07 21:05:53 +0200

    push exercice après correction et commentaires

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-function getTheRightPassWord(){
-  if (document.getElementById('field1').value == document.getElementById('field2').value)
-    {
-    document.getElementById('field1').style.border= '2px solid green';
-    document.getElementById('field2').style.border= '2px solid green';
-    }
-  else{
-    document.getElementById('field1').style.border="2px solid red";
-    document.getElementById('field2').style.border= '2px solid red';
-    alert('Veillez saisir deux mots de passe identiques !');
-    }
+function getTheRightPassWord() {
+	if (document.getElementById('field1').value == document.getElementById('field2').value) {
+		document.getElementById('field1').style.border = '2px solid green';
+		document.getElementById('field2').style.border = '2px solid green';
+	} else {
+		document.getElementById('field1').style.border = "2px solid red";
+		document.getElementById('field2').style.border = '2px solid red';
+		alert('Veillez saisir deux mots de passe identiques !');
+	}
 }
